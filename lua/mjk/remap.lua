d864d64e334e6ee6ef20895660f2f65b793b81dc
Matthew Kenkel <mattsnoopy2@gmail.com>
2023-04-16 18:48:05 -0400

    Push

--- lua/mjk/remap.lua
+++ lua/mjk/remap.lua
@@ -1,2 +1,4 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>ed", vim.cmd.Ex) -- While in normal mode, if I press leader pv, exec the following. ed = escape dir
+vim.keymap.set("n", "<leader>..", vim.cmd.bnext) -- While in normal mode, if I press leader pv, exec the following. ed = escape dir
+vim.keymap.set("n", "<leader>,,", vim.cmd.bprev) -- While in normal mode, if I press leader pv, exec the following. ed = escape dir
