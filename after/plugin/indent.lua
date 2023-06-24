0aa4f7b2654743c53dcc1ad8f424ecced1dece5f
Matthew Kenkel <mattsnoopy2@gmail.com>
2023-06-24 13:19:18 -0400

    Workflow update

--- after/plugin/indent.lua
+++ after/plugin/indent.lua
@@ -8,18 +8,8 @@ vim.cmd [[highlight IndentBlanklineIndent6 guifg=#39c5cf gui=nocombine]]
 
 vim.opt.list = true
 vim.opt.listchars:append "space:⋅"
-vim.opt.listchars:append "eol:↴"
 
 require("indent_blankline").setup {
-    space_char_blankline = " ",
-    space_char_highlight_list = {
-        "IndentBlanklineIndent1",
-        "IndentBlanklineIndent2",
-        "IndentBlanklineIndent3",
-        "IndentBlanklineIndent4",
-        "IndentBlanklineIndent5",
-        "IndentBlanklineIndent6",
-    },
     show_current_context = true,
     show_current_context_start = true
 }
