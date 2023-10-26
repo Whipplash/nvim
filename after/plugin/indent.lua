ebc3611a2d7e3ec14aa30072d47c70197ad11cd8
Whipplash <mattsnoopy2@gmail.com>
2023-10-26 09:23:35 -0400

    IndentLine v3

--- after/plugin/indent.lua
+++ after/plugin/indent.lua
@@ -1,18 +1,26 @@
-vim.opt.termguicolors = true
-vim.cmd [[highlight IndentBlanklineIndent1 guifg=#ea4a5a gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent2 guifg=#34d058 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent3 guifg=#ffea7f gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent4 guifg=#2188ff gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent5 guifg=#b392f0 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent6 guifg=#39c5cf gui=nocombine]]
-
-vim.opt.list = true
-vim.opt.listchars:append "space:⋅"
-
-require("ibl").setup {
-    show_current_context = true,
-    show_current_context_start = true,
-    strict_tabs = true,
-    buftype_exclude = { 'terminal', 'nofile', 'quickfix' },
-    bufname_exclude = { ' ' }
+local highlight = {
+    "RainbowRed",
+    "RainbowYellow",
+    "RainbowBlue",
+    "RainbowOrange",
+    "RainbowGreen",
+    "RainbowViolet",
+    "RainbowCyan",
 }
+local hooks = require "ibl.hooks"
+-- create the highlight groups in the highlight setup hook, so they are reset
+-- every time the colorscheme changes
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+    vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+    vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+    vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+    vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+    vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+    vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+    vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+end)
+
+vim.g.rainbow_delimiters = { highlight = highlight }
+require("ibl").setup { scope = { highlight = highlight } }
+
+hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
